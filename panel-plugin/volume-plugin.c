becbdf3cd316654a1c772aefdbf86693524f8d13
cjheo <cjheo@gooroom.kr>
2018-02-20 11:37:35 +0900

    Using the gtk2 library instead of gtk3 as a stability issue in the xfce4 panel
    Change-Id: Ie8e8ab9260f6079ad2f603535812afbafde4095a

--- panel-plugin/volume-plugin.c
+++ panel-plugin/volume-plugin.c
@@ -31,6 +31,8 @@
 #endif
 
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
+
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4panel/xfce-panel-plugin.h>
 #include <keybinder.h>
@@ -39,17 +41,17 @@
 #include <libnotify/notify.h>
 
 #include "volume-plugin.h"
-#include "volume-resources.h"
 
 #include "pulseaudio-config.h"
 #include "pulseaudio-volume.h"
 
+#define PANEL_TRAY_ICON_SIZE            22
 
 #define VOLUME_PLUGIN_RAISE_VOLUME_KEY  "XF86AudioRaiseVolume"
 #define VOLUME_PLUGIN_LOWER_VOLUME_KEY  "XF86AudioLowerVolume"
 #define VOLUME_PLUGIN_MUTE_KEY          "XF86AudioMute"
 
-#define GET_WIDGET(builder, x) GTK_WIDGET (gtk_builder_get_object (builder, x))
+
 
 
 struct _VolumePluginClass
@@ -64,8 +66,6 @@ struct _VolumePlugin
 
 	PulseaudioConfig  *config;
 	PulseaudioVolume  *volume;
-
-	GtkBuilder        *builder;
 
 	GtkWidget         *button;
 	GtkWidget         *img_tray;
@@ -90,8 +90,7 @@ notify_notification (NotifyNotification *notification,
     GdkPixbuf *pix = NULL;
 
 	pix = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (),
-                                    icon,
-                                    32,
+                                    icon, 32,
                                     GTK_ICON_LOOKUP_FORCE_SIZE,
                                     NULL);
 
@@ -108,16 +107,16 @@ notify_volume_notification (NotifyNotification *notification,
 	const gchar *icon;
 
 	if (muted) {
-		icon = "audio-volume-muted-symbolic";
+		icon = "gooroom-volume-muted-symbolic";
 	} else {
 		if (volume== 0) {
-			icon = "audio-volume-low-symbolic";
+			icon = "gooroom-volume-low-symbolic";
 		} else if (volume < 34) {
-			icon = "audio-volume-low-symbolic";
+			icon = "gooroom-volume-low-symbolic";
 		} else if (volume < 67) {
-			icon = "audio-volume-medium-symbolic";
+			icon = "gooroom-volume-medium-symbolic";
 		} else {
-			icon = "audio-volume-high-symbolic";
+			icon = "gooroom-volume-high-symbolic";
 		}
 	}
 
@@ -157,25 +156,19 @@ volume_plugin_volume_key_pressed (const char *keystring, gpointer data)
 static void
 volume_plugin_mute_pressed (const char *keystring, gpointer data)
 {
-	gboolean muted;
 	VolumePlugin *plugin = VOLUME_PLUGIN (data);
 
 	gdouble volume = pulseaudio_volume_get_volume (plugin->volume);
+	gboolean muted = pulseaudio_volume_get_muted (plugin->volume);
 
-	if (pulseaudio_volume_get_muted (plugin->volume)) {
-		muted = FALSE;
-	} else {
-		muted = TRUE;
-	}
-
-	pulseaudio_volume_set_muted (plugin->volume, muted);
+	pulseaudio_volume_set_muted (plugin->volume, !muted);
 
 	/* Play a sound! */
 	ca_gtk_play_for_widget (GTK_WIDGET (plugin), 0,
 			CA_PROP_EVENT_ID, "audio-volume-change",
 			NULL);
 
-	notify_volume_notification (plugin->notification, muted, volume * 100.0);
+	notify_volume_notification (plugin->notification, !muted, volume * 100.0);
 
 	pulseaudio_volume_toggle_muted (plugin->volume);
 }
@@ -221,7 +214,35 @@ volume_plugin_bind_keys_cb (PulseaudioConfig *config, gpointer data)
 }
 
 static void
-volume_button_update (VolumePlugin *plugin, gboolean force_update)
+popup_volume_button_icon_update (VolumePlugin *plugin)
+{
+	gboolean     muted;
+	gboolean     connected;
+	const gchar *icon_name;
+
+	connected = pulseaudio_volume_get_connected (plugin->volume);
+	muted = pulseaudio_volume_get_muted (plugin->volume);
+
+	if (!connected || muted) {
+		icon_name = "gooroom-volume-muted-symbolic";
+	} else {
+		gdouble volume = pulseaudio_volume_get_volume (plugin->volume);
+
+		if (volume <= 0.0)
+			icon_name = "gooroom-volume-zero-symbolic";
+		else if (volume <= 0.3)
+			icon_name = "gooroom-volume-low-symbolic";
+		else if (volume <= 0.7)
+			icon_name = "gooroom-volume-medium-symbolic";
+		else
+			icon_name = "gooroom-volume-high-symbolic";
+	}
+
+	gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), icon_name, GTK_ICON_SIZE_BUTTON);
+}
+
+static void
+volume_button_icon_update (VolumePlugin *plugin, gboolean force_update)
 {
 	gboolean     connected;
 	gboolean     muted;
@@ -231,22 +252,30 @@ volume_button_update (VolumePlugin *plugin, gboolean force_update)
 	connected = pulseaudio_volume_get_connected (plugin->volume);
 
 	if (!connected || muted) {
-		icon_name = "audio-volume-muted-panel-symbolic";
+		icon_name = "gooroom-volume-muted-panel-symbolic";
 	} else {
 		gdouble volume = pulseaudio_volume_get_volume (plugin->volume);
 		if (volume <= 0.0)
-			icon_name = "audio-volume-zero-panel-symbolic";
+			icon_name = "gooroom-volume-zero-panel-symbolic";
 		else if (volume <= 0.3)
-			icon_name = "audio-volume-low-panel-symbolic";
+			icon_name = "gooroom-volume-low-panel-symbolic";
 		else if (volume <= 0.7)
-			icon_name = "audio-volume-medium-panel-symbolic";
+			icon_name = "gooroom-volume-medium-panel-symbolic";
 		else
-			icon_name = "audio-volume-high-panel-symbolic";
+			icon_name = "gooroom-volume-high-panel-symbolic";
 	}
 
 	if (force_update || icon_name != plugin->icon_name) {
 		plugin->icon_name = icon_name;
-		gtk_image_set_from_icon_name (GTK_IMAGE (plugin->img_tray), icon_name, GTK_ICON_SIZE_LARGE_TOOLBAR);
+
+		GdkPixbuf *pix = NULL;
+		pix = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (),
+                     icon_name, PANEL_TRAY_ICON_SIZE, GTK_ICON_LOOKUP_FORCE_SIZE, NULL);
+
+		if (pix) {
+			gtk_image_set_from_pixbuf (GTK_IMAGE (plugin->img_tray), pix);
+			g_object_unref (G_OBJECT (pix));
+		}
 	}
 }
 
@@ -255,15 +284,12 @@ on_mute_button_clicked (GtkToggleButton *button, gpointer data)
 {
 	VolumePlugin *plugin = VOLUME_PLUGIN (data);
 
-	if (pulseaudio_volume_get_muted (plugin->volume)) {
-		gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), "audio-volume-high-symbolic", GTK_ICON_SIZE_BUTTON);
-		gtk_widget_set_sensitive (plugin->scale, TRUE);
-		pulseaudio_volume_set_muted (plugin->volume, FALSE);
-	} else {
-		gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), "audio-volume-muted-symbolic", GTK_ICON_SIZE_BUTTON);
-		gtk_widget_set_sensitive (plugin->scale, FALSE);
-		pulseaudio_volume_set_muted (plugin->volume, TRUE);
-	}
+	gboolean muted = pulseaudio_volume_get_muted (plugin->volume);
+
+	gtk_widget_set_sensitive (plugin->scale, muted);
+	pulseaudio_volume_set_muted (plugin->volume, !muted);
+
+	popup_volume_button_icon_update (plugin);
 
 	/* Play a sound! */
 	ca_gtk_play_for_widget (GTK_WIDGET (plugin), 0,
@@ -306,17 +332,11 @@ on_volume_changed (PulseaudioVolume *volume, gpointer data)
 
 	gtk_range_set_value (GTK_RANGE (plugin->scale), pulseaudio_volume_get_volume (volume) * 100.0);
 
-	if (pulseaudio_volume_get_muted (volume)) {
-		if (plugin->popup_vol_icon)
-			gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), "audio-volume-muted-symbolic", GTK_ICON_SIZE_BUTTON);
-	} else {
-		if (plugin->popup_vol_icon)
-			gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), "audio-volume-high-symbolic", GTK_ICON_SIZE_BUTTON);
-	}
+	popup_volume_button_icon_update (plugin);
 
 	g_signal_handlers_unblock_by_func (G_OBJECT (plugin->scale), on_scale_value_changed, data);
 
-	volume_button_update (plugin, FALSE);
+	volume_button_icon_update (plugin, FALSE);
 }
 
 static void
@@ -347,7 +367,7 @@ on_popup_window_closed (gpointer data)
 static gboolean
 on_popup_key_press_event (GtkWidget *widget, GdkEventKey *event, gpointer data)
 {
-	if (event->type == GDK_KEY_PRESS && event->keyval == GDK_KEY_Escape) {
+	if (event->type == GDK_KEY_PRESS && event->keyval == GDK_Escape) {
 		on_popup_window_closed (data);
 		return TRUE;
 	}
@@ -367,47 +387,64 @@ close_popup_window (gpointer data)
 }
 
 static GtkWidget *
-popup_window_new (VolumePlugin *plugin)
+popup_volume_window (VolumePlugin *plugin)
 {
-	GError    *error = NULL;
-	GdkScreen *screen;
-	GtkWidget *window, *button;
+	GtkWidget *window;
 
-	gtk_builder_add_from_resource (plugin->builder, "/kr/gooroom/volume/plugin/volume-window.ui", &error);
-	if (error) {
-		g_error_free (error);
-		return NULL;
-	}
-
-	window = GET_WIDGET (plugin->builder, "volume-window");
+	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_type_hint (GTK_WINDOW (window), GDK_WINDOW_TYPE_HINT_UTILITY);
 	gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
 	gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
 	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
-	gtk_window_set_skip_pager_hint (GTK_WINDOW (window), TRUE);
+	gtk_window_set_skip_pager_hint(GTK_WINDOW (window), TRUE);
 	gtk_window_set_keep_above (GTK_WINDOW (window), TRUE);
-	gtk_window_stick (GTK_WINDOW (window));
+	gtk_window_stick(GTK_WINDOW (window));
+	gtk_window_set_screen (GTK_WINDOW (window), gtk_widget_get_screen (GTK_WIDGET (plugin)));
 
 	gtk_widget_add_events (window, GDK_KEY_PRESS_MASK);
 
-	if (gtk_widget_has_screen (GTK_WIDGET (plugin->button)))
-		screen = gtk_widget_get_screen (GTK_WIDGET (plugin->button));
-	else
-		screen = gdk_display_get_default_screen (gdk_display_get_default ());
+	GtkWidget *main_vbox = gtk_vbox_new (FALSE, 0);
+	gtk_container_add (GTK_CONTAINER (window), main_vbox);
 
-	gtk_window_set_screen (GTK_WINDOW (window), screen);
+	GtkWidget *ebox = gtk_event_box_new ();
+	gtk_widget_set_name (ebox, "panel-popup-window-frame");
+	gtk_box_pack_start (GTK_BOX (main_vbox), ebox, FALSE, FALSE, 0);
 
-	button                 = GET_WIDGET (plugin->builder, "btn-volume");
-	plugin->scale          = GET_WIDGET (plugin->builder, "scale-volume");
-	plugin->popup_vol_icon = GET_WIDGET (plugin->builder, "popup-vol-icon");
+	GtkWidget *alignment = gtk_alignment_new (0.0, 0.5, 0.0, 0.0);
+	gtk_alignment_set_padding (GTK_ALIGNMENT (alignment), 5, 5, 5, 5);
+	gtk_container_add (GTK_CONTAINER (ebox), alignment);
 
-	if (pulseaudio_volume_get_muted (plugin->volume)) {
-		gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), "audio-volume-muted-symbolic", GTK_ICON_SIZE_BUTTON);
-		gtk_widget_set_sensitive (plugin->scale, FALSE);
-	} else {
-		gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), "audio-volume-high-symbolic", GTK_ICON_SIZE_BUTTON);
-		gtk_widget_set_sensitive (plugin->scale, TRUE);
-	}
+	GtkWidget *title = gtk_label_new (NULL);
+	gtk_label_set_markup (GTK_LABEL (title), _("<big><b>Volume Control</b></big>"));
+	gtk_label_set_justify (GTK_LABEL (title), GTK_JUSTIFY_LEFT);
+	gtk_container_add (GTK_CONTAINER (alignment), title);
+
+	GtkWidget *hbox = gtk_hbox_new (FALSE, 6);
+	gtk_container_set_border_width (GTK_CONTAINER (hbox), 12);
+	gtk_box_pack_start (GTK_BOX (main_vbox), hbox, FALSE, FALSE, 0);
+
+	GtkWidget *button = gtk_button_new ();
+	gtk_button_set_relief (GTK_BUTTON (button), GTK_RELIEF_NONE);
+	gtk_widget_set_can_focus (button, FALSE);
+	gtk_box_pack_start (GTK_BOX (hbox), button, FALSE, FALSE, 0);
+
+	plugin->popup_vol_icon = gtk_image_new_from_icon_name ("gooroom-volume-muted-symbolic", GTK_ICON_SIZE_BUTTON);
+	gtk_container_add (GTK_CONTAINER (button), plugin->popup_vol_icon);
+
+	plugin->scale = gtk_hscale_new_with_range (0.0, 100.0, 1.0);
+	gtk_widget_set_can_focus (plugin->scale, FALSE);
+	gtk_widget_set_size_request (plugin->scale, 224, -1);
+	gtk_range_set_inverted (GTK_RANGE (plugin->scale), FALSE);
+	gtk_scale_set_draw_value (GTK_SCALE (plugin->scale), FALSE);
+	gtk_range_set_round_digits (GTK_RANGE (plugin->scale), 0);
+	gtk_box_pack_start (GTK_BOX (hbox), plugin->scale, TRUE, TRUE, 0);
+
+	gboolean muted = pulseaudio_volume_get_muted (plugin->volume);
+
+	const gchar *icon_name = muted ? "gooroom-volume-muted-symbolic" : "gooroom-volume-high-symbolic";
+
+	gtk_image_set_from_icon_name (GTK_IMAGE (plugin->popup_vol_icon), icon_name, GTK_ICON_SIZE_BUTTON);
+	gtk_widget_set_sensitive (plugin->scale, !muted);
 
 	g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (on_mute_button_clicked), plugin);
 	g_signal_connect (G_OBJECT (plugin->scale), "value-changed", G_CALLBACK (on_scale_value_changed), plugin);
@@ -423,7 +460,7 @@ popup_window_new (VolumePlugin *plugin)
 	else
 		gtk_widget_set_sensitive (window, FALSE);
 
-	gtk_widget_show (window);
+	gtk_widget_show_all (window);
 
 	xfce_panel_plugin_block_autohide (XFCE_PANEL_PLUGIN (plugin), TRUE);
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (plugin->button), TRUE);
@@ -435,6 +472,19 @@ popup_window_new (VolumePlugin *plugin)
 }
 
 static gboolean
+on_volume_button_icon_update_timeout (gpointer data)
+{
+	VolumePlugin *plugin = VOLUME_PLUGIN (data);
+
+	volume_button_icon_update (plugin, TRUE);
+
+	g_signal_connect (G_OBJECT (plugin->volume), "volume-changed", G_CALLBACK (on_volume_changed), plugin);
+
+	return FALSE;
+}
+
+
+static gboolean
 on_volume_button_pressed (GtkWidget *widget, GdkEventButton *event, gpointer data)
 {
 	VolumePlugin *plugin = VOLUME_PLUGIN (data);
@@ -444,7 +494,7 @@ on_volume_button_pressed (GtkWidget *widget, GdkEventButton *event, gpointer dat
 			if (plugin->popup_window != NULL) {
 				on_popup_window_closed (plugin);
 			} else {
-				plugin->popup_window = popup_window_new (plugin);
+				plugin->popup_window = popup_volume_window (plugin);
 			}
 
 			return TRUE;
@@ -455,21 +505,6 @@ on_volume_button_pressed (GtkWidget *widget, GdkEventButton *event, gpointer dat
 	return (*GTK_WIDGET_CLASS (volume_plugin_parent_class)->button_press_event) (GTK_WIDGET (plugin), event);
 }
 
-static gboolean
-lazy_display_volume (gpointer data)
-{
-	VolumePlugin *plugin = VOLUME_PLUGIN (data);
-
-	plugin->img_tray = gtk_image_new ();
-	gtk_container_add (GTK_CONTAINER (plugin->button), plugin->img_tray);
-
-	volume_button_update (plugin, TRUE);
-
-	gtk_widget_show (plugin->img_tray);
-
-	return FALSE;
-}
-
 static void
 volume_plugin_free_data (XfcePanelPlugin *panel_plugin)
 {
@@ -477,9 +512,6 @@ volume_plugin_free_data (XfcePanelPlugin *panel_plugin)
 
 	/* release keybindings */
 	volume_plugin_unbind_keys (plugin);
-
-	if (plugin->builder)
-		g_object_unref (plugin->builder);
 
 	close_popup_window (plugin);
 }
@@ -514,9 +546,32 @@ volume_plugin_init (VolumePlugin *plugin)
 	plugin->popup_vol_icon = NULL;
 	plugin->notification   = NULL;
 
-	g_resources_register (volume_get_resource ());
+	plugin->button = xfce_panel_create_toggle_button ();
+	xfce_panel_plugin_add_action_widget (XFCE_PANEL_PLUGIN (plugin), plugin->button);
+	gtk_container_add (GTK_CONTAINER (plugin), plugin->button);
 
-	plugin->builder = gtk_builder_new ();
+	GdkPixbuf *pix = NULL;
+	pix = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (),
+                                    "gooroom-volume-high-panel-symbolic", PANEL_TRAY_ICON_SIZE,
+                                    GTK_ICON_LOOKUP_FORCE_SIZE, NULL);
+
+	if (pix) {
+		plugin->img_tray = gtk_image_new_from_pixbuf (pix);
+		gtk_container_add (GTK_CONTAINER (plugin->button), plugin->img_tray);
+		g_object_unref (G_OBJECT (pix));
+	}
+
+	gtk_widget_show_all (plugin->button);
+
+	g_signal_connect (G_OBJECT (plugin->button), "button-press-event", G_CALLBACK (on_volume_button_pressed), plugin);
+}
+
+static void
+volume_plugin_construct (XfcePanelPlugin *panel_plugin)
+{
+	VolumePlugin *plugin = VOLUME_PLUGIN (panel_plugin);
+
+	xfce_textdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
 
 	plugin->config = pulseaudio_config_new (xfce_panel_plugin_get_property_base (XFCE_PANEL_PLUGIN (plugin)));
 	plugin->volume = pulseaudio_volume_new (plugin->config);
@@ -533,25 +588,8 @@ volume_plugin_init (VolumePlugin *plugin)
 		volume_plugin_bind_keys (plugin);
 	else
 		volume_plugin_unbind_keys (plugin);
-}
 
-static void
-volume_plugin_construct (XfcePanelPlugin *panel_plugin)
-{
-	VolumePlugin *plugin = VOLUME_PLUGIN (panel_plugin);
-
-	xfce_textdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
-
-	plugin->button = xfce_panel_create_toggle_button ();
-	gtk_button_set_relief (GTK_BUTTON (plugin->button), GTK_RELIEF_NONE);
-	gtk_container_add (GTK_CONTAINER (plugin), plugin->button);
-	xfce_panel_plugin_add_action_widget (XFCE_PANEL_PLUGIN (plugin), plugin->button);
-	gtk_widget_show (plugin->button);
-
-	g_timeout_add (200, (GSourceFunc)lazy_display_volume, plugin);
-
-	g_signal_connect (G_OBJECT (plugin->button), "button-press-event", G_CALLBACK (on_volume_button_pressed), plugin);
-	g_signal_connect (G_OBJECT (plugin->volume), "volume-changed", G_CALLBACK (on_volume_changed), plugin);
+	g_timeout_add (200, (GSourceFunc) on_volume_button_icon_update_timeout, plugin);
 }
 
 static void
